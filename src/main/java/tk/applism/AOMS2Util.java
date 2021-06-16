5e976a42c6257100e32ea9d6f7cf400ad3874ba5
applearon <57509063+applearon@users.noreply.github.com>
2021-06-15 20:30:00 -0400

    Initial Commit

---
+++ src/main/java/tk/applism/AOMS2Util.java
@@ -0,0 +1,51 @@
+package tk.applism;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
+
+public class AOMS2Util extends JavaPlugin implements Listener{
+    @Override
+    public void onEnable() {
+        getLogger().info("Applism Origin Minecraft Server Two Utils Plugin has been enabled!");
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+    @Override
+    public void onDisable() {
+        getLogger().info("onDisable has been invoked!");
+    }
+
+    //JumpPad
+    public static void JumpMethod(Material BelowBlock, Player player, int vx, int vy, int vz) {
+        if(player.getLocation().getBlock().getRelative(BlockFace.DOWN).getType().equals(Material.SLIME_BLOCK) && player.getLocation().getBlock().getRelative(BlockFace.DOWN).getRelative(BlockFace.DOWN).getType().equals(BelowBlock)) {
+            Vector v =  player.getVelocity();
+            v.setX(vx);
+            v.setY(vy);
+            v.setZ(vz);
+            player.setVelocity(v);
+        }
+    }
+    //Set LowerMaterial and power of JumpPad
+    @EventHandler
+    public void onJump(PlayerMoveEvent event) {
+        JumpMethod(Material.REDSTONE_BLOCK, event.getPlayer(), 0, 3, 5);
+        JumpMethod(Material.LAPIS_BLOCK, event.getPlayer(), 0, 3, -5);
+    }
+
+    //VoidProtect
+    @EventHandler
+    public void onMovement(PlayerMoveEvent event) {
+        if (event.getTo().getBlockY() < 0) {
+            Location TeleportArea = event.getTo();
+            //Sets Protect Height
+            TeleportArea.setY(128);
+            event.getPlayer().teleport(TeleportArea);
+        }
+    }
+}
